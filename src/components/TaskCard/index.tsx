7157237fe251a99a453d77c1646fd1e22a8fd461
marcelosantana <marcelosantana@alu.ufc.br>
2023-02-07 14:11:23 -0300

    [To-Do-App] - Insert CheckBox

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,11 +1,30 @@
-import { Feather } from "@expo/vector-icons";
+import { Feather, AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { TaskCardContainer, TaskDescription } from "./styles";
+import {
+  CheckBox,
+  TaskCardContainer,
+  TaskDescription,
+  TaskSelect,
+} from "./styles";
 
 export function TaskCard() {
+  const [checked, setChecked] = useState(false);
+
+  function check() {
+    setChecked(!checked);
+  }
+
   return (
     <TaskCardContainer>
-      <TaskDescription>Primeira atividade</TaskDescription>
+      <TaskSelect>
+        <CheckBox onPress={check}>
+          {checked === true && (
+            <AntDesign name="checkcircle" size={13.8} color="#5E60CE" />
+          )}
+        </CheckBox>
+        <TaskDescription>Primeira atividade com descrição.</TaskDescription>
+      </TaskSelect>
       <TouchableOpacity>
         <Feather name="trash-2" size={16} color="white" />
       </TouchableOpacity>
--- src/components/TaskCard/styles.ts
+++ src/components/TaskCard/styles.ts
@@ -7,9 +7,26 @@ export const TaskCardContainer = styled.View`
   padding: 10px;
   background-color: ${(props) => props.theme.colors["gray-400"]};
   border-radius: 8px;
+  height: 54px;
+`;
+
+export const TaskSelect = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CheckBox = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  width: 16px;
+  height: 16px;
+  border-radius: 9999px;
+  border: 1px solid ${(props) => props.theme.colors.blue};
 `;
 
 export const TaskDescription = styled.Text`
   color: ${(props) => props.theme.colors.white};
   font-size: 12px;
+  margin-left: 10px;
 `;
